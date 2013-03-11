68530f1bd3b652fee9c1031714567d8dd370b31b
Matt Spaulding <mspaulding06@gmail.com>
2013-03-11 14:05:57 -0700

    Updated build-rpm script to not use rpmfab

--- build-rpm.sh
+++ build-rpm.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-if [ ! -d ./rpmfab ]; then
-    git clone git://github.com/gholms/rpmfab.git
+set -x
+
+[ -d ./build ] && rm -rf build
+
+mkdir -p build/{BUILD,BUILDROOT,SRPMS,RPMS,SOURCES,SPECS}
+mkdir results
+rpm -ivh --define "_topdir `pwd`/build" *.src.rpm
+
+if [ "$1" = "devel" ]; then
+    rpmbuild --define "_topdir `pwd`/build" \
+        --define "dist .el6" --define "devbuild 1" \
+        -ba build/SPECS/*.spec
+else
+    rpmbuild --define "_topdir `pwd`/build" \
+        --define "dist .el6" \
+        -ba build/SPECS/*.spec
 fi
 
-rpmfab/build-arch.py -r epel-6-x86_64 -o results \
-    --mock-options "--uniqueext $BUILD_TAG" *.src.rpm
+find build/ -name "*.rpm" -exec mv {} results/ \;
 
